0ced1483058980f0e5a1bf8ff9e758061f5345e4
Matija <matija.komorcec@gmail.com>
2020-02-05 20:40:04 +0100

    First commit

---
+++ WordUnscrambler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 7, 3, 5, 4, 15 };
+
+            Array.Sort(myArray);
+        }
+    }
+}
